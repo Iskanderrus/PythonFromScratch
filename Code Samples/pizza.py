cc52177bd1fa9bf86163cc2aa6471c4f5c630c89
alex <a.n.chasovskoy@gmail.com>
2023-10-11 12:46:31 +0200

    chapter 8 - styling the function

--- Code Samples/pizza.py
+++ Code Samples/pizza.py
@@ -1,6 +1,12 @@
-def make_pizza(size, *toppings):
-    """Summarize the pizza we are about to make"""
+def make_pizza(size: int, *toppings: str) -> None:
+    """
+    Print summarized pizza size and toppings for the pizza we are about to make.
+    Size is a mandatory argument.
+    Toppings can vary from 0 to infinity.
+    """
+
     print(f'\nMaking a {size}-inch pizza with following topics: ')
+
     for topping in toppings:
         print(f'\t- {topping}')
     print(f"\n{'*' * 32}")
