13dca811f24dba23ca8d278897c807e7920ec3ba
alex <a.n.chasovskoy@gmail.com>
2023-10-22 00:42:47 +0200

    chapter 9 - closing commit

--- Code Samples/Python Standard Library/lottery.py
+++ Code Samples/Python Standard Library/lottery.py
@@ -7,20 +7,22 @@ numbers = string.digits
 equals = False
 counter = 0
 
-symbols_list = []
-for _ in range(10):
-    symbols_list.append(choice(characters))
-
-for _ in range(5):
-    symbols_list.append(choice(numbers))
-
-shuffle(symbols_list)
-my_winning_combination = sample(symbols_list, 4)
 
 while not equals:
+    symbols_list = []
+    for _ in range(10):
+        symbols_list.append(choice(characters))
+
+    for _ in range(5):
+        symbols_list.append(choice(numbers))
+
+    shuffle(symbols_list)
+    my_winning_combination = "".join(sample(symbols_list, 4))
+
     winning_symbols = sample(symbols_list, 4)
     winning_combination = "".join(winning_symbols)
     print(winning_combination)
+    counter += 1
     if my_winning_combination == winning_combination:
         equals = True
 print(counter)
