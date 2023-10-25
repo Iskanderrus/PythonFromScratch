aa27a7b0ea98f92dce98a18dd7cb38133d662053
alex <a.n.chasovskoy@gmail.com>
2023-10-25 17:09:45 +0200

    chapter 10 - refactored version of the remember_me.py file

--- Code Samples/Chapter 10/remember_me.py
+++ Code Samples/Chapter 10/remember_me.py
@@ -1,13 +1,48 @@
 from pathlib import Path
 import json
 
-path = Path('username.json')
-if path.exists():
-    contents = path.read_text()
-    stored_name = json.loads(contents)
-    print(f"Hi, {stored_name}! Nice to see you again!")
-else:
+
+def get_stored_username(path_object: Path) -> str or None:
+    """
+    Function to extract previously saved username.
+    :param path_object: Path object for file containing username.
+    :return: Username as a string value or None object if path object doesn't exist.
+    """
+
+    if path_object.exists():
+        path_contents = path_object.read_text()
+        stored_name = json.loads(path_contents)
+        return stored_name
+    else:
+        return None
+
+
+def get_new_username(path_object: Path) -> str:
+    """
+    Function to get username of a new user and save it in the json file.
+    :param path_object: Path object to save the username in json file.
+    :return: Username as a string.
+    """
     username = input('What is your name? ')
     contents = json.dumps(username)
-    path.write_text(contents)
-    print(f"We'll remember youi when you come back, {username}!")
+    path_object.write_text(contents)
+    return username
+
+
+def greet_user() -> None:
+    """
+    Function to greet user if username was saved in the json file. Or store the username in json file if it is new.
+    :return: None
+    """
+
+    path = Path('username.json')
+    user_name = get_stored_username(path)
+    if user_name:
+        print(f"Hi, {user_name}! Nice to see you again!")
+
+    else:
+        username = get_new_username(path)
+        print(f"We'll remember you when you come back, {username}!")
+
+
+greet_user()
