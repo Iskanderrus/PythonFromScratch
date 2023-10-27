53e8a8b7c2ef08b802c9496cce92e7c1474137af
alex <a.n.chasovskoy@gmail.com>
2023-10-28 00:34:12 +0200

    chapter 11 - Exercise 11.3 - solution with fixture providing an employee instance to each of the test functions

--- Code Samples/Chapter 11/Exercise 11_3/test_employee.py
+++ Code Samples/Chapter 11/Exercise 11_3/test_employee.py
@@ -2,13 +2,17 @@ import pytest
 from employee import Employee
 
 
-def test_give_default_raise():
+@pytest.fixture
+def some_employee():
     some_employee = Employee('John', 'Smith', 10_000)
+    return some_employee
+
+
+def test_give_default_raise(some_employee):
     some_employee.give_raise()
     assert some_employee.annual_salary == 60_000
 
 
-def test_give_custom_raise():
-    some_employee = Employee('John', 'Smith', 10_000)
+def test_give_custom_raise(some_employee):
     some_employee.give_raise(annual_raise=35_000)
     assert some_employee.annual_salary == 45_000
