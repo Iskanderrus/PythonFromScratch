620faec8d795532c9a9a8759d30ad61a97f7a3ec
alex <a.n.chasovskoy@gmail.com>
2023-10-29 21:52:54 +0100

    chapter 12 - Aliens invasion game the clock setup

--- Code Samples/aliens_invasion/alien_invasion.py
+++ Code Samples/aliens_invasion/alien_invasion.py
@@ -9,6 +9,7 @@ class AlienInvasion:
         """ Initialize the game and create game resources. """
 
         pygame.init()
+        self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption('Alien Invasion')
 
@@ -21,6 +22,7 @@ class AlienInvasion:
                     sys.exit()
             # Make the most recently drawn screen visible.
             pygame.display.flip()
+            self.clock.tick(60)
 
 
 if __name__ == '__main__':
