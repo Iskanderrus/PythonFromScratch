29c54598e142da8e1846ce10748ba94f1e234be0
alex <a.n.chasovskoy@gmail.com>
2023-10-29 22:08:14 +0100

    chapter 12 - Aliens invasion game settings file added

--- Code Samples/aliens_invasion/alien_invasion.py
+++ Code Samples/aliens_invasion/alien_invasion.py
@@ -1,6 +1,6 @@
 import sys
 import pygame
-
+from settings import Settings
 
 class AlienInvasion:
     """ Overall class to manage game assets and behavior. """
@@ -10,9 +10,10 @@ class AlienInvasion:
 
         pygame.init()
         self.clock = pygame.time.Clock()
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption('Alien Invasion')
-        self.bg_color = (30, 35, 150)
+        #self.bg_color = (50, 18 ,122)
 
     def run_game(self):
         """ Start the main loop for the game. """
@@ -22,7 +23,7 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
             # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
             # Make the most recently drawn screen visible.
             pygame.display.flip()
             self.clock.tick(60)
