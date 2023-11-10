5467f15623a669eb22fa16b921cc9733fa33ab46
alex <a.n.chasovskoy@gmail.com>
2023-11-10 23:22:27 +0100

    Chapter 15 - upgrading the graph

--- Code Samples/Chapter 15/mpl_squares.py
+++ Code Samples/Chapter 15/mpl_squares.py
@@ -3,6 +3,14 @@ import matplotlib.pyplot as plt
 sqares = [1, 4, 9, 16, 25]
 
 fig, ax = plt.subplots()
-ax.plot(sqares)
+ax.plot(sqares, linewidth=3)
+
+# Set chart title and label axes
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+# Set size of tick labels
+ax.tick_params(labelsize=14)
 
 plt.show()
