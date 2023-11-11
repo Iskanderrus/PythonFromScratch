3216bc47e4c21cc933ff60f9479122dbdc4c7a66
alex <a.n.chasovskoy@gmail.com>
2023-11-11 22:01:07 +0100

    Chapter 15 - scatter graph - added variables and set range for both axes

--- Code Samples/Chapter 15/scatter_squares.py
+++ Code Samples/Chapter 15/scatter_squares.py
@@ -1,16 +1,21 @@
 import matplotlib.pyplot as plt
 
+x_values = [x for x in range(1, 101) if x % 2 == 0]
+y_values = [x ** 2 for x in range(1, 101) if x % 2 == 0]
+
 plt.style.use('seaborn-v0_8-dark-palette')
 fig, ax = plt.subplots()
-ax.scatter([x for x in range(1, 11) if x % 2 == 0],
-           [x ** 2 for x in range(1, 11) if x % 2 == 0])
+ax.scatter(x_values, y_values, s=10)
 
 # Set chart title and label axes
-ax.set_title("Square Numbers", fontsize=24)
+ax.set_title("Square Even Numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Square of Value", fontsize=14)
 
 # Set size of tick labels
 ax.tick_params(labelsize=14)
+
+# Set the range for each axis
+ax.axis([0, 110, 0, 11_000])
 
 plt.show()
