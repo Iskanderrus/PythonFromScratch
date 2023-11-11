ae73895c8d5e3aa4ecfbbcb44d099c05780f3132
alex <a.n.chasovskoy@gmail.com>
2023-11-11 23:13:33 +0100

    Chapter 15 - Random Walk visualisation upon request. Main parameters are being user defined

--- Code Samples/Chapter 15/random_walk/rw_visual.py
+++ Code Samples/Chapter 15/random_walk/rw_visual.py
@@ -2,16 +2,24 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-# Make a random walk
-rw = RandomWalk(num_points=50000, num_directions=12)
-rw.fill_walk()
 
-# Plot the points of the walk
+while True:
+    num_points = int(input("How many points? "))
+    num_directions = int(input("How many directions? "))
+    # Make a random walk
+    rw = RandomWalk(num_points=num_points, num_directions=num_directions)
+    rw.fill_walk()
 
-plt.style.use('seaborn-v0_8-dark-palette')
-fig, ax = plt.subplots()
+    # Plot the points of the walk
 
-ax.scatter(rw.x_values, rw.y_values, c=rw.y_values, cmap=plt.cm.plasma, s=2.5)
-ax.set_aspect('equal')
-plt.show()
+    plt.style.use('seaborn-v0_8-dark-palette')
+    fig, ax = plt.subplots()
 
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.plasma, edgecolors='none', s=2.5)
+    ax.set_aspect('equal')
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n)").strip().lower()
+    if keep_running == 'n':
+        break
