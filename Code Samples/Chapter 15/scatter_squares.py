199737ef02fc582a6f7548e5d3823a79ed69333f
alex <a.n.chasovskoy@gmail.com>
2023-11-11 22:09:48 +0100

    Chapter 15 - scatter graph - added colormap to style the scatter plot

--- Code Samples/Chapter 15/scatter_squares.py
+++ Code Samples/Chapter 15/scatter_squares.py
@@ -5,7 +5,7 @@ y_values = [x ** 2 for x in range(1, 101) if x % 2 == 0]
 
 plt.style.use('seaborn-v0_8-dark-palette')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=10)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.gist_ncar, s=10)
 
 # Set chart title and label axes
 ax.set_title("Square Even Numbers", fontsize=24)
@@ -17,5 +17,6 @@ ax.tick_params(labelsize=14)
 
 # Set the range for each axis
 ax.axis([0, 110, 0, 11_000])
+ax.ticklabel_format(style='plain')
 
 plt.show()
