182f64f3d9c587de2f86544b1cc2be829a4e9055
alex <a.n.chasovskoy@gmail.com>
2023-11-12 21:51:43 +0100

    Chapter 15 - Rolling Die with Plotly - Die rolling visualized

--- Code Samples/Chapter 15/plotly_rolling_dice/die_visual.py
+++ Code Samples/Chapter 15/plotly_rolling_dice/die_visual.py
@@ -3,13 +3,16 @@ from die import Die
 
 # Create a die instance with 6 sides
 die = Die(num_sides=int(input("How many sides has your die? ")))
-
+rolls = int(input("How many times do you want to roll the die? "))
 # Make some rolls, and store results in a list
-results = [die.roll() for x in range(1000)]
+results = [die.roll() for x in range(rolls)]
 
 # Analyse the results
 frequencies = [results.count(value) for value in range(1, die.num_sides+1)]
 
 # Visualize the results
-fig = px.bar(x=range(1, die.num_sides+1), y=frequencies)
+title = f"Results of Rolling One Die with {die.num_sides} Sides {rolls} Times"
+labels = {'x': "Result",
+          'y': "Frequency of Result"}
+fig = px.bar(x=range(1, die.num_sides+1), y=frequencies, title=title, labels=labels)
 fig.show()
