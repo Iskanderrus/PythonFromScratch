b1094c9f21d9c1f3e794f53826e5b365a9a7362e
alex <a.n.chasovskoy@gmail.com>
2023-11-12 20:26:06 +0100

    Chapter 15 - Random Walk - 15.3

---
+++ Code Samples/Chapter 15/random_walk/rw_plot.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+from random import choice
+from random_walk import RandomWalk
+
+while True:
+    num_points = int(input("How many points? "))
+    num_directions = int(input("How many directions? "))
+    # Make a random walk
+    rw = RandomWalk(num_points=num_points, num_directions=num_directions)
+    rw.fill_walk()
+
+    # Plot the points of the walk
+
+    plt.style.use('seaborn-v0_8-dark-palette')
+    fig, ax = plt.subplots(figsize=(15, 9))
+
+    ax.plot(rw.x_values, rw.y_values, color='orange', alpha=0.6)
+    # Remove axes
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    ax.set_aspect('equal')
+    ax.scatter(0, 0, c='green', edgecolors='none', s=50)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=50)
+
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n)").strip().lower()
+    if keep_running == 'n':
+        break
