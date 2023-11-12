bf81f5bce61dee2b9fe563c8a6df184247a250a1
alex <a.n.chasovskoy@gmail.com>
2023-11-12 21:57:54 +0100

    Chapter 15 - Rolling Die with Plotly - Two Dies Rolling Visualization

--- Code Samples/Chapter 15/plotly_rolling_dice/die_visual.py
+++ Code Samples/Chapter 15/plotly_rolling_dice/die_visual.py
@@ -1,18 +1,22 @@
 import plotly.express as px
 from die import Die
 
-# Create a die instance with 6 sides
-die = Die(num_sides=int(input("How many sides has your die? ")))
+# Create a die instance
+sides = int(input("How many sides have your first and second dies? "))
+die_1 = Die(num_sides=sides)
+die_2 = Die(num_sides=sides)
+
 rolls = int(input("How many times do you want to roll the die? "))
+
 # Make some rolls, and store results in a list
-results = [die.roll() for x in range(rolls)]
+results = [die_1.roll() + die_2.roll() for x in range(rolls)]
 
 # Analyse the results
-frequencies = [results.count(value) for value in range(1, die.num_sides+1)]
+frequencies = [results.count(value) for value in range(1, die_1.num_sides+die_2.num_sides+1)]
 
 # Visualize the results
-title = f"Results of Rolling One Die with {die.num_sides} Sides {rolls} Times"
+title = f"Results of Rolling Two Dies with {die_1.num_sides} Sides {rolls} Times"
 labels = {'x': "Result",
           'y': "Frequency of Result"}
-fig = px.bar(x=range(1, die.num_sides+1), y=frequencies, title=title, labels=labels)
+fig = px.bar(x=range(1, die_1.num_sides + die_2.num_sides+1), y=frequencies, title=title, labels=labels)
 fig.show()
