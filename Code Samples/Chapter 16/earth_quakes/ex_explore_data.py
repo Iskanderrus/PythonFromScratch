2dba74b983ba73dc10e3962f88b6ff8a8aebde94
alex <a.n.chasovskoy@gmail.com>
2023-11-13 21:09:11 +0100

    Chapter 16 - Mapping the earthquakes data - added a hover-over information

--- Code Samples/Chapter 16/earth_quakes/ex_explore_data.py
+++ Code Samples/Chapter 16/earth_quakes/ex_explore_data.py
@@ -17,14 +17,16 @@ all_eq_data = json.loads(contents)
 all_eq_dicts = all_eq_data['features']
 
 # Extracting Magnitudes and Location Data
-mags, lons, lats = [], [], []
+mags, lons, lats, eq_titles = [], [], [], []
 for eq_dict in all_eq_dicts:
     mag = abs(eq_dict['properties']['mag'])
     lon = eq_dict['geometry']['coordinates'][0]
     lat = eq_dict['geometry']['coordinates'][1]
+    eq_title = eq_dict['properties']['title']
     mags.append(mag)
     lons.append(lon)
     lats.append(lat)
+    eq_titles.append(eq_title)
 
 # Mapping the earthquakes
 title = "Global Earthquakes - Oct-Nov 2023"
@@ -36,5 +38,6 @@ fig = px.scatter_geo(lat=lats,
                      color_continuous_scale='Viridis',
                      labels={'color': 'Magnitude'},
                      projection='natural earth',
+                     hover_name=eq_titles,
                      )
 fig.show()
