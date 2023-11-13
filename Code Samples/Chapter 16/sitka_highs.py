16bded1eddc9dcbbb60ff1df3003cd763bfa6d3c
alex <a.n.chasovskoy@gmail.com>
2023-11-13 12:49:04 +0100

    Chapter 16 - Plotting the high and low temperatures graph with formatted dates

--- Code Samples/Chapter 16/sitka_highs.py
+++ Code Samples/Chapter 16/sitka_highs.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import csv
+from datetime import datetime
 
 import matplotlib.pyplot as plt
 
@@ -10,20 +11,30 @@ lines = path.read_text().splitlines()
 reader = csv.reader(lines)
 header_row = next(reader)
 
-for index, column_header in enumerate(header_row):
-    print(index, column_header)
+# for index, column_header in enumerate(header_row):
+#     print(index, column_header)
 
 # Extract high temperatures
-highs = [int(row[4]) for row in reader]
+
+dates, highs, lows = [], [], []
+for row in reader:
+    current_date = datetime.strptime(row[2], '%Y-%m-%d')
+    high = int(row[4])
+    low = int(row[5])
+    dates.append(current_date)
+    highs.append(high)
+    lows.append(low)
 
 # Plot the high temperatures
 plt.style.use('seaborn-v0_8-dark-palette')
 fig, ax = plt.subplots()
-ax.plot(highs, color='red')
+ax.plot(dates, highs, color='red')
+ax.plot(dates, lows, color='blue')
 
 # Formatting the plot
-ax.set_title("Daily High Temperatures, July 2021", fontsize=24)
+ax.set_title("Daily High and Low Temperatures, July 2021", fontsize=24)
 ax.set_xlabel('', fontsize=16)
+fig.autofmt_xdate()
 ax.set_ylabel("Temperature (F)", fontsize=16)
 ax.tick_params(labelsize=16)
 
