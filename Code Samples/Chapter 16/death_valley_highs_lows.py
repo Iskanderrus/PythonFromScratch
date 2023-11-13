8d5bfa59f90936cce41afbd64f71fcc687ee05f2
alex <a.n.chasovskoy@gmail.com>
2023-11-13 20:29:41 +0100

    Chapter 16 - Handling of missing values

--- Code Samples/Chapter 16/death_valley_highs_lows.py
+++ Code Samples/Chapter 16/death_valley_highs_lows.py
@@ -1,29 +1,27 @@
+from datetime import datetime
 from pathlib import Path
 import csv
-from datetime import datetime
 
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 
-
-path = Path('data/sitka_weather_2021_simple.csv')
+path = Path('data/death_valley_2021_simple.csv')
 lines = path.read_text().splitlines()
 
 reader = csv.reader(lines)
 header_row = next(reader)
 
-# for index, column_header in enumerate(header_row):
-#     print(index, column_header)
-
-# Extract high temperatures
-
 dates, highs, lows = [], [], []
 for row in reader:
     current_date = datetime.strptime(row[2], '%Y-%m-%d')
-    high = int(row[4])
-    low = int(row[5])
-    dates.append(current_date)
-    highs.append(high)
-    lows.append(low)
+    try:
+        high = int(row[3])
+        low = int(row[4])
+    except ValueError:
+        print(f"Missint data for {current_date}")
+    else:
+        dates.append(current_date)
+        highs.append(high)
+        lows.append(low)
 
 # Plot the high temperatures
 plt.style.use('seaborn-v0_8-dark-palette')
@@ -33,7 +31,7 @@ ax.plot(dates, lows, color='blue')
 ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # Formatting the plot
-ax.set_title("Daily High and Low Temperatures in Sitka, 2021", fontsize=24)
+ax.set_title("Daily High and Low Temperatures, 2021\nDeath Valley, CA", fontsize=20)
 ax.set_xlabel('', fontsize=16)
 fig.autofmt_xdate()
 ax.set_ylabel("Temperature (F)", fontsize=16)
