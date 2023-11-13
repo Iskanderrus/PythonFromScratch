9b4ac12fc38215ade5a7a001470e2670a841d417
alex <a.n.chasovskoy@gmail.com>
2023-11-13 20:49:15 +0100

    Chapter 16 - Extracting magnitude and location data from the geojson file

--- Code Samples/Chapter 16/earth_quakes/ex_explore_data.py
+++ Code Samples/Chapter 16/earth_quakes/ex_explore_data.py
@@ -7,6 +7,19 @@ contents = path.read_text()
 all_eq_data = json.loads(contents)
 
 # Create more readable file with data
-path = Path('data/readable_eq_data.geojson')
-readable_contents = json.dumps(all_eq_data, indent=4)
-path.write_text(readable_contents)
+# path = Path('data/readable_eq_data.geojson')
+# readable_contents = json.dumps(all_eq_data, indent=4)
+# path.write_text(readable_contents)
+
+# Examine all earthquakes in the dataset
+all_eq_dicts = all_eq_data['features']
+
+# Extracting Magnitudes and Location Data
+mags, lons, lats = [], [], []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
+    mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
