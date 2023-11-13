d453e2355c0f62f402f5555127786b84601db768
alex <a.n.chasovskoy@gmail.com>
2023-11-13 21:01:33 +0100

    Chapter 16 - Mapping the earthquakes data

--- Code Samples/Chapter 16/earth_quakes/ex_explore_data.py
+++ Code Samples/Chapter 16/earth_quakes/ex_explore_data.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import json
 
+import plotly.express as px
+
 # Read data as a string and convert to a Python object
 path = Path('data/all_month.geojson')
 contents = path.read_text()
@@ -17,9 +19,22 @@ all_eq_dicts = all_eq_data['features']
 # Extracting Magnitudes and Location Data
 mags, lons, lats = [], [], []
 for eq_dict in all_eq_dicts:
-    mag = eq_dict['properties']['mag']
+    mag = abs(eq_dict['properties']['mag'])
     lon = eq_dict['geometry']['coordinates'][0]
     lat = eq_dict['geometry']['coordinates'][1]
     mags.append(mag)
     lons.append(lon)
     lats.append(lat)
+
+# Mapping the earthquakes
+title = "Global Earthquakes - Oct-Nov 2023"
+fig = px.scatter_geo(lat=lats,
+                     lon=lons,
+                     size=mags,
+                     title=title,
+                     color=mags,
+                     color_continuous_scale='Viridis',
+                     labels={'color': 'Magnitude'},
+                     projection='natural earth',
+                     )
+fig.show()
