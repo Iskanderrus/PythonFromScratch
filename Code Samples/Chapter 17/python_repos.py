e4d16269a651ee0d4e9b09bb30284c1a2362b2da
alex <a.n.chasovskoy@gmail.com>
2023-11-14 20:59:16 +0100

    Chapter 17 - Processing the response dictionaries, exploring the first dictionary

--- Code Samples/Chapter 17/python_repos.py
+++ Code Samples/Chapter 17/python_repos.py
@@ -18,7 +18,16 @@ r = requests.get(url=python_api_request, headers=headers)
 print(f"Status: {r.status_code}")
 
 # Convert the response object to a dictionary
-response_dict = r.json()
+response_dicts = r.json()
+print(f"Total repositories: {response_dicts['total_count']}")
+print(f"Complete results: {not response_dicts['incomplete_results']}")
 
-# Process results
-print(response_dict.keys())
+# Explore information about the repositories
+repo_dicts = response_dicts['items']
+print(f"Repositories returned: {len(repo_dicts)}")
+
+# Ecamine the first repository
+repo_dict = repo_dicts[0]
+print(f"\nKeys: {len(repo_dict)}")
+for key in sorted(repo_dict.keys()):
+    print(key)
