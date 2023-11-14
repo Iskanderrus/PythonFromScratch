2a720f4e888aecc85f6ec26b2dcf64c14057e2e7
alex <a.n.chasovskoy@gmail.com>
2023-11-14 21:51:11 +0100

    Chapter 17 - Plotting the request results

--- Code Samples/Chapter 17/python_repos.py
+++ Code Samples/Chapter 17/python_repos.py
@@ -1,5 +1,6 @@
 import requests
 from pathlib import Path
+import plotly.express as px
 
 # Making an API call and checking the response
 token_path = Path('../../../../Documents/github_token.txt')
@@ -24,10 +25,30 @@ print(f"Complete results: {not response_dicts['incomplete_results']}")
 
 # Explore information about the repositories
 repo_dicts = response_dicts['items']
+repo_names, stars = [], []
 print(f"Repositories returned: {len(repo_dicts)}")
 
-# Ecamine the first repository
-repo_dict = repo_dicts[0]
-print(f"\nKeys: {len(repo_dict)}")
-for key in sorted(repo_dict.keys()):
-    print(key)
+print('\nSelected information about each repository: ')
+for repo_dict in repo_dicts:
+    repo_names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+    print(f"\nName: {repo_dict['name']}")
+    print(f"Owner: {repo_dict['owner']['login']}")
+    print(f"Stars: {repo_dict['stargazers_count']}")
+    print(f"Repository: {repo_dict['html_url']}")
+    print(f"Description: {repo_dict['description']}")
+
+fig = px.bar(x=repo_names,
+             y=stars,
+             hover_data=[repo_names, stars],
+             color=stars,
+             #text_auto=True,
+             labels={'y':'Number of Stars', 'x': 'Repositories'},
+             title='Top Python Repositories in GitHub')
+
+fig.update_traces(hovertemplate=None)
+fig.update_layout(hovermode="x",
+                  title_font_size=28,
+                  xaxis_title_font_size=20,
+                  yaxis_title_font_size=20)
+fig.write_html('top_python_repos_in_github.html')
