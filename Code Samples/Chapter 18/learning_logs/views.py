0e2e90cf6da23804e08736b0e1c8d32bdd953023
alex <a.n.chasovskoy@gmail.com>
2023-11-21 19:32:58 +0100

    Chapter 18 - Learning Log - protecting user's data and associating the new topic with the logged-in user

--- Code Samples/Chapter 18/learning_logs/views.py
+++ Code Samples/Chapter 18/learning_logs/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, redirect
-from .models import Topic, Entry
-from .forms import TopicForm, EntryForm
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
+from django.shortcuts import render, redirect
+
+from .forms import TopicForm, EntryForm
+from .models import Topic, Entry
 
 
 # Create your views here.
@@ -22,6 +24,11 @@ def topics(request):
 def topic(request, topic_id):
     """ Show a single topic and all its entries """
     topic = Topic.objects.get(id=topic_id)
+
+    #Make sure the topic belongs to the current user
+    if topic.owner != request.user:
+        raise Http404
+
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
@@ -37,7 +44,9 @@ def new_topic(request):
         # POST data submitted; process data
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('learning_logs:topics')
     # Display a blank or invalid form
     context = {'form': form}
@@ -70,6 +79,8 @@ def edit_entry(request, entry_id):
     """ Edit an existing entry """
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    if topic.owner != request.user:
+        raise Http404
 
     if request.method != 'POST':
         # Initial request; pre-fill form with the current entry
